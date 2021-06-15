add65583048b1862bb66df11ae22ae6bcdd772ac
Pedro <pedromiotti7@gmail.com>
2021-06-15 20:01:20 -0300

    ID bug

--- src/main/java/espminvest/poo/user/common/controller/UserController.java
+++ src/main/java/espminvest/poo/user/common/controller/UserController.java
@@ -11,18 +11,21 @@ import java.util.List;
 @FeignClient("service.user")
 public interface UserController {
 
-    @PostMapping(path = "/users", consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+    public static final String BASE_URL = "/api/v1/users";
+
+    // Accepting xxx-form-urlencoded
+    @PostMapping(path = BASE_URL, consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE,
             MediaType.MULTIPART_FORM_DATA_VALUE}, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public UserBean saveUser(@RequestBody @ModelAttribute UserBean user);
 
-    @GetMapping(path = "/users/{id}")
+    @GetMapping(path = BASE_URL + "/{id}")
     public UserBean getUserById(@PathVariable String id);
 
-    @GetMapping(path = "/users")
+    @GetMapping(path = BASE_URL)
     public List<UserBean> getUsers();
 
-    @DeleteMapping(path = "/users/{id}")
+    @DeleteMapping(path = BASE_URL + "/{id}")
     public void deleteUser(@PathVariable String id);
 
 }
--- src/main/java/espminvest/poo/user/common/datatype/UserBean.java
+++ src/main/java/espminvest/poo/user/common/datatype/UserBean.java
@@ -8,6 +8,10 @@ public class UserBean {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
